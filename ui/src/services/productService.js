9ae04cd9cb616d558ee8ee9af0b46cec411d0ef4
StrappedGlint13 <matiasbrax@gmail.com>
2021-12-09 15:09:15 +0200

    load balancing to nginx

--- ui/src/services/productService.js
+++ ui/src/services/productService.js
@@ -1,22 +1,11 @@
 import axios from 'axios'
-import { SERVER_1, SERVER_2, SERVER_3 } from '../utils/config'
-
-const balanceLoad = () => {
-  let servers = [ SERVER_1, SERVER_2, SERVER_3 ] // tähän servereiden haku
-  const randomNumber = Math.floor(Math.random()*servers.length)
-  return servers[randomNumber]
-}
 
 export const getProducts = async () => {
-  const server = balanceLoad()
-  console.log(server)
-  const products = await axios.get(server)
+  const products = await axios.get("http://localhost/")
   return products.data
 }
 
 export const createProduct = async newObject => {
-  const server = balanceLoad()
-  console.log(server)
-  const response = await axios.post(server + '/product', newObject)
+  const response = await axios.post('http://localhost/product', newObject)
   return response.data
 }
